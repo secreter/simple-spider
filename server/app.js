8f611e540ddbf2d6e1b1d2dd5824a01f8bd1aa8e
So <1979510177@qq.com>
2018-03-09 00:06:09 +0800

    pass page

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 const puppeteer = require("puppeteer");
+const utils = require("./lib/utils");
 
-(async () => {
-  const browser = await puppeteer.launch();
+const main = async () => {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
-
+  await page.goto(
+    "http://tj.58.com/shangpucz/pn1/?sourcetype=5&area=3000_%2A&sq=1"
+  );
+  // await page.goto('http://example.com')
+  // await page.screenshot({ path: 'example.png' })
+  let urls = await utils.getPageAHrefs(page);
+  console.log(urls);
   await browser.close();
-})();
+};
 
-console.log(11);
+main()
+  .then(() => {})
+  .catch(e => {
+    console.error(e);
+  });
