8904ed6c0b9c270e6cdac9304d141abd91dfafb2
pengchaoyang <pengchaoyang@sogou-inc.com>
2018-03-09 11:50:31 +0800

    daily

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://example.com')
+  await page.screenshot({ path: 'example.png' })
 
-  await browser.close();
-})();
+  await browser.close()
+})()
 
-console.log(11);
+console.log(11)
