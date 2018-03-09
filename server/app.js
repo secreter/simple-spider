4525501cccf15f52108eda995295cfa8504c75b2
So <1979510177@qq.com>
2018-03-09 22:02:24 +0800

    daily

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
-const puppeteer = require("puppeteer");
-const utils = require("./lib/utils");
+const puppeteer = require('puppeteer')
+const utils = require('./lib/utils')
 
 const main = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
   await page.goto(
-    "http://tj.58.com/shangpucz/pn1/?sourcetype=5&area=3000_%2A&sq=1"
-  );
+    'http://tj.58.com/shangpucz/pn1/?sourcetype=5&area=3000_%2A&sq=1'
+  )
   // await page.goto('http://example.com')
   // await page.screenshot({ path: 'example.png' })
-  let urls = await utils.getPageAHrefs(page);
-  console.log(urls);
-  await browser.close();
-};
+  let urls = await utils.getPageAHrefs(page)
+  console.log(urls)
+  await browser.close()
+}
 
 main()
   .then(() => {})
   .catch(e => {
-    console.error(e);
-  });
+    console.error(e)
+  })
